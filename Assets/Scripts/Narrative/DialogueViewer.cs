2f5c9734b4794b435090edd92f0640766d4c54e9
biniidea <biniidea@naver.com>
2023-03-09 11:30:45 -0500

    Move to narrativebranch

---
+++ Assets/Scripts/Narrative/DialogueViewer.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static DialogueObject;
+using UnityEngine.Events;
+using System;
+using System.Runtime.InteropServices;
+using TMPro;
+public class DialogueViewer : MonoBehaviour
+{
+    [SerializeField] Transform parentOfResponses;
+    [SerializeField] Button prefab_btnResponse;
+    //[SerializeField] SlowTyper txtMessage;
+    [SerializeField] TextMeshProUGUI txtMessage;
+    //[SerializeField] UnityEngine.UI.Text txtMessage;
+
+    //[SerializeField] SlowTyper txtTitle;
+    //[SerializeField] Image imgMemory;
+    //[SerializeField] Button btnSpeedyProgress;
+    [SerializeField] DialogueController dialogueController;
+    DialogueController controller;
+    [DllImport("__Internal")]
+    private static extern void openPage(string url);
+
+    private void Start()
+    {
+        controller = dialogueController;
+        controller.onEnteredNode += OnNodeEntered;
+        controller.InitializeDialogue();
+
+        // Start the dialogue
+        var curNode = controller.GetCurrentNode();
+        //Debug.Log(curNode.text);
+
+    }
+
+    public static void KillAllChildren(UnityEngine.Transform parent)
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(parent);
+        for (int childIndex = parent.childCount - 1; childIndex >= 0; childIndex--)
+        {
+            UnityEngine.Object.Destroy(parent.GetChild(childIndex).gameObject);
+        }
+    }
+
+    private void OnNodeSelected(int indexChosen)
+    {
+        Debug.Log("Chose: " + indexChosen);
+        controller.ChooseResponse(indexChosen);
+    }
+
+    private void OnNodeEntered(Node newNode)
+    {
+        Debug.Log("Entering Node: " + newNode.title);
+
+
+        //txtMessage.text = newNode.text;
+        KillAllChildren(parentOfResponses);
+        
+            for (int i = newNode.responses.Count - 1; i >= 0; i--)
+            {
+                Debug.Log("i["+i.ToString()+"], newNode.responses.Count["+newNode.responses.Count.ToString()+"]");
+                int currentChoiceIndex = i;
+                var response = newNode.responses[i];
+                var responceButton = Instantiate(prefab_btnResponse, parentOfResponses);
+                responceButton.GetComponentInChildren<TextMeshProUGUI>().text = response.displayText;
+
+                txtMessage.text = response.messageText;
+                //Debug.Log(newNode.text);
+                Debug.Log("DisplayText["+response.displayText+"]");
+                responceButton.onClick.AddListener(delegate { OnNodeSelected(currentChoiceIndex); });
+            }
+        
+        //Debug.Log("End");
+
+        //txtMessage.text = "aaaaa";
+
+    }
+}
---
+++ Assets/Scripts/Narrative/NarrativeManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NarrativeManager : MonoBehaviour
+{
+    private static int MAXBRANCHES = 3;
+
+    private static int MAXOPTIONS = 3;
+
+    private string sampleDialogue = "This is the sample dialogue in manager";
+
+    private string[,] inGameDialogues = new string[MAXOPTIONS, MAXBRANCHES];
+    public GameObject dialogueObj;
+
+    void Start()
+    {
+        //dialogueObj.GetComponent<TMPro.TextMeshProUGUI>().text = sampleDialogue;
+        SetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetUp()
+    {
+        inGameDialogues[0, 0] = "This is the branch 1 in first option";
+        inGameDialogues[0, 1] = "This is the branch 2 in first option";
+        inGameDialogues[0, 2] = "This is the branch 3 in first option";
+
+        inGameDialogues[0, 0] = "This is the branch 1 in second option";
+        inGameDialogues[0, 1] = "This is the branch 2 in second option";
+        inGameDialogues[0, 2] = "This is the branch 3 in second option";
+    }
+
+    public void ChooseBranchInChoice(int option, int branch)
+    {
+        dialogueObj.GetComponent<TMPro.TextMeshProUGUI>().text = inGameDialogues[option, branch];
+    }
+}
